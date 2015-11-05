7bae991ad1386af0a77083be1a59b2cfa6445dff
Marcel Engelmann <engelmann.marcel@yahoo.de>
2015-11-05 23:23:50 +0100

    Add platform shell script

---
+++ shell-scripts/add-platforms.sh
@@ -0,0 +1,10 @@
+## Add platforms to the ionic project
+
+echo 'Add platforms to the ionic project'
+echo '############################################'
+echo 'Add iOS platform to the ionic project'
+cordova platform add ios
+echo 'Add Android platform to the ionic project'
+cordova platform add android
+echo 'Add browser platform to the ionic project'
+cordova platform add browser
