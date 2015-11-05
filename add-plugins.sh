83a4efd8a7eb297b0d85cd947367d0e0dc76c673
Marcel Engelmann <engelmann.marcel@yahoo.de>
2015-11-05 23:18:42 +0100

    Shell script for adding plugins

---
+++ add-plugins.sh
@@ -0,0 +1,24 @@
+## Shell Script to download all plugins for the App
+echo 'Add Crosswalk WebView plugin'
+cordova plugins add cordova-plugin-crosswalk-webview
+
+echo 'Add Crosswalk Whitelist plugin'
+cordova plugins add cordova-plugin-whitelist
+
+echo 'Add Crosswalk Cordova Native Audio plugin'
+cordova plugins add de.neofonie.cordova.plugin.nativeaudio
+
+echo 'Add Crosswalk File plugin'
+cordova plugins add cordova-plugin-file
+
+echo 'Add Crosswalk InAppBrowser plugin'
+cordova plugins add cordova-plugin-inappbrowser
+
+echo 'Add Crosswalk Media plugin'
+cordova plugins add cordova-plugin-media
+
+echo 'Add Crosswalk Splashscreen plugin'
+cordova plugins add cordova-plugin-splashscreen
+
+echo 'Add Crosswalk Text to speech plugin'
+cordova plugins add cordova-plugin-tts
